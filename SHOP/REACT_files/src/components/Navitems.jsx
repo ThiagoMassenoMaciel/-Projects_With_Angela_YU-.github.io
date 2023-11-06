64d270fd77de8e09d73775232e0ca2f47a43b51d
ThiagoMassenoMaciel <maciel72210764@gmail.com>
2023-11-06 06:06:10 -0300

    SHOP- watched 10' i waste 1h doing

--- SHOP/REACT_files/src/components/Navitems.jsx
+++ SHOP/REACT_files/src/components/Navitems.jsx
@@ -1,10 +1,55 @@
 import React, { useState } from 'react'
-
+import { Link } from 'react-router-dom'
+import logo from '../assets/images/logo/logo.png'
 const Navitems = () => {
   const [menuToggle, setMenuToggle] = useState(false)
-  const [first, setfirst] = useState(second)
+  const [socialToggle, setSocialToggle] = useState(false)
+  const [headerFixed, setHeaderFixed] = useState(false)
+
+  //add event listenner
+  window.addEventListener("scroll", ()=> { 
+    // When you scroll the viewport on flow direction Vertical(Y) i will set the Position fixed
+    if (window.scrollY > 200){
+        setHeaderFixed(true)
+    }else{
+        setHeaderFixed(false)
+    }
+  })
+
   return (
-    <div>Navitems</div>
+    <header className={`header-section style-4 ${headerFixed ? "header-fixed fadeInUp" : ""}`}>
+      {/*   header top start */}
+
+      <div className ={ `header-top d-md-none ${socialToggle ? "open": ""}`} >
+          <div className="container">
+          <div className="header-top-area">
+              <Link to="/signup" className="lab-btn me-3"><span>Create Accout</span></Link>
+              <Link to="/login"><span>Log in</span></Link>
+          </div>
+        </div>
+      </div>
+
+    {/*   header button */}
+
+      <div className="header-bottom">
+        <div className="container">
+          <div className="header-wrapper">
+            {/*   logo  */}
+
+            <div className="logo-search-acte">
+              <div className="logo">
+                <Link to={"/"}>
+                  <img src={logo} alt="This image is a logo for the site eccommerse" />
+                </Link>
+              </div>
+                  
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </header>
   )
 }
 
